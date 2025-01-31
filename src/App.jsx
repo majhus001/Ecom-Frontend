bdcb88521f9698e27716ad312b79863aa1f0985a
Majid Husain <majidsmart7@gmail.com>
2025-01-31 09:57:54 +0530

    latest31

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ProductAddPage from './Admin/Productadd';
+import HomePage from './screens/Homepage/HomePage';
+import SignUp from './LogSin/SignUp';
+import Login from './LogSin/Login';
+import ProductList from './screens/Productscreens/ProductList';
+import Cart from './screens/Productscreens/Cart';
+import Navbar from './screens/navbar/Navbar';
+import Orderdetails from './screens/Order/Orderdetails';
+import Orderhistory from './screens/Order/Orderhistory';
+import Sidebar from './screens/sidebar/Sidebar';
+import ProfilePage from './screens/profiledetails/ProfilePage';
+import AdminHome from './Admin/homepage/AdminHome';
+import Adnavbar from './Admin/Adnavbar/Adnavbar';
+import Adminorders from './Admin/Adminorders/Adminorders';
+import Adminprofile from './Admin/Adminprofile/Adminprofile';
+import Searchproducts from './screens/searchproducts/Searchproducts';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        {/* Set initial route */}
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/navbar" element={<Navbar />} />
+        <Route path="/prodlist" element={<ProductList />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product" element={<ProductPage />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/adprod" element={<ProductAddPage />} />
+        <Route path="/orderdet" element={<Orderdetails />} />
+        <Route path="/myorders" element={<Orderhistory />} />
+        <Route path="/side" element={<Sidebar />} />
+        <Route path="/profilepage" element={<ProfilePage />} />
+        <Route path="/adhome" element={<AdminHome />} />
+        <Route path="/adnavbar" element={<Adnavbar />} />
+        <Route path="/adorders" element={<Adminorders />} />
+        <Route path="/adprof" element={<Adminprofile />} />
+        <Route path="/seprodlist" element={<Searchproducts />} />
+
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
