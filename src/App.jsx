a2b47f9515730b4a6f22a56ed5fac2952d8b1ce8
Majid Husain <majidsmart7@gmail.com>
2025-01-31 10:37:58 +0530

    ll

--- src/App.jsx
+++ src/App.jsx
@@ -21,12 +21,11 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        {/* Set initial route */}
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
         <Route path="/navbar" element={<Navbar />} />
         <Route path="/prodlist" element={<ProductList />} />
-        {/* <Route path="/homep" element={<HomePage />} /> */}
+        <Route path="/home" element={<HomePage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/adprod" element={<ProductAddPage />} />
